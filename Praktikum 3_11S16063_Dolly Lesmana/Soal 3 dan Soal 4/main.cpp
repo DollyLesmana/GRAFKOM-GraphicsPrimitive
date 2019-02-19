23f3ec2c34add93be79d9fc523d751af889130e4
Dolly Lesmana Lubis <47165953+DollyLesmana@users.noreply.github.com>
2019-02-19 20:11:25 +0700

    Add files via upload

---
+++ Praktikum 3_11S16063_Dolly Lesmana/Soal 3 dan Soal 4/main.cpp
@@ -0,0 +1,64 @@
+/*11S16063*/
+/*Praktikum 3*/
+/*DOLLY LESMANA*/
+
+
+
+
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <GL/glut.h>
+#include <math.h>
+
+
+#include <stdlib.h> //Load library
+#include <math.h>   //Load library aritmatik
+
+void lingkar (int posx, int posy, int r)
+{
+    glBegin(GL_POLYGON);
+    for (int i=0;i<360 ;i++)
+    {
+        float sudut=i*(2*3.14/360);
+        float x=posx+r*cos(sudut);
+        float y=posy+r*sin(sudut);
+        glVertex2f(x,y);
+    }
+
+    glEnd();
+    glFlush();
+}
+
+void renderScene ()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    /* ....*/
+    glColor3f(0.,0.,1.);
+    lingkar(-70,20,50);//kiri-kanan,atas-bawah,besar-kecil
+    glColor3f(1.,1.,0.);
+    lingkar(300,250,100);
+    glColor3f(0.,1.,1.);
+    lingkar(170,120,80);
+    glColor3f(1.,0.,1.);
+    lingkar(-170,120,105);
+    glColor3f(0.,1.,0.);
+    lingkar(190,-110,180);
+    glColor3f(1.,0.,0.);
+    lingkar(50,-60,50);
+}
+
+int main (int argc, char **argv)
+{
+    glutInit (&argc, argv);
+    /* ... */
+    glutInitDisplayMode (GLUT_DEPTH | GLUT_SINGLE | GLUT_RGBA);
+    glutInitWindowPosition(150,150);
+    glutInitWindowSize(640,440);
+    glutCreateWindow("2D KOMPLEKS DDA and BRESENHAM PEMBENTUK LINGKARAN MIDPOINT");
+        gluOrtho2D(-640.,640.,-440.,440.);
+    glutDisplayFunc(renderScene);
+    glutMainLoop();
+}
